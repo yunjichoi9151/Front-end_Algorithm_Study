63880f3d12ffdcb77c102c39ac66aa0671bbf535
yunjichoi9151 <cyjcode99@gmail.com>
2023-06-25 16:27:51 +0900

    [BOJ] 막대기 23.06.25 CYJ

---
+++ Algorithm/CYJ/막대기_17608.java
@@ -0,0 +1,26 @@
+package Algorithm.CYJ;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class 막대기_17608 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Stack<Integer> stack = new Stack<>();
+		int n = sc.nextInt();
+		int ans = 1;
+		int[] arr = new int[n];
+		for(int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		stack.push(arr[n - 1]);
+		for(int i = n - 2; i >= 0; i--) {
+			if(arr[i] > stack.peek()) {
+				stack.push(arr[i]);
+				ans++;
+			}
+		}
+		System.out.println(ans);
+		sc.close();
+	}
+}
