849203be9e326a779483087720f0f6dfe9512fa4
yunjichoi9151 <cyjcode99@gmail.com>
2023-07-02 00:03:51 +0900

    [BOJ] Hook 23.07.02 CYJ

---
+++ Algorithm/CYJ/Hook_10189.java
@@ -0,0 +1,7 @@
+package Algorithm.CYJ;
+
+public class Hook_10189 {
+    public static void main(String[] args) {
+        System.out.println("#  # #### #### #  #\n#### #  # #  # # #\n#### #  # #  # # #\n#  # #### #### #  #");
+    }
+}
