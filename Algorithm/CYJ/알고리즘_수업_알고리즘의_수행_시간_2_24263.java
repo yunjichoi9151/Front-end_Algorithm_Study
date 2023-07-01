7af2630c259d61ad29e477c6fd24bfbfd7eab2f7
yunjichoi9151 <cyjcode99@gmail.com>
2023-07-02 00:11:38 +0900

    [BOJ] 알고리즘 수업 - 알고리즘의 수행 시간 2 23.07.02 CYJ

---
+++ Algorithm/CYJ/알고리즘_수업_알고리즘의_수행_시간_2_24263.java
@@ -0,0 +1,12 @@
+package Algorithm.CYJ;
+
+import java.util.Scanner;
+
+public class 알고리즘_수업_알고리즘의_수행_시간_2_24263 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n + "\n" + 1);
+        sc.close();
+    }
+}
