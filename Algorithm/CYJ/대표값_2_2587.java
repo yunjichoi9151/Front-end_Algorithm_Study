b18d5184d815cc7ea715c64c1c7fb2d960162c45
yunjichoi9151 <cyjcode99@gmail.com>
2023-07-09 04:09:15 +0900

    [BOJ] 대표값 2 23.07.09 CYJ

---
+++ Algorithm/CYJ/대표값_2_2587.java
@@ -0,0 +1,19 @@
+package Algorithm.CYJ;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 대표값_2_2587 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        int sum = 0;
+        for(int i = 0; i < 5; i++) {
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+        Arrays.sort(arr);
+        System.out.println(sum / 5 + "\n" + arr[2]);
+        sc.close();
+    }
+}
