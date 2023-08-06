492f7103e10a2c88486b9bdbbc7bf0a8033d24dd
yunjichoi9151 <cyjcode99@gmail.com>
2023-08-06 15:57:53 +0900

    [PRGRMS] 소수 찾기 23.08.06 CYJ

---
+++ Algorithm/CYJ/Programmers/소수_찾기_12921.java
@@ -0,0 +1,17 @@
+package Programmers;
+
+public class 소수_찾기_12921 {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 3; i <= n; i++) {
+            answer++;
+            for(int j = 2; j <= i / j; j++) {
+                if(i % j == 0) {
+                    answer--;
+                    break;
+                }
+            }
+        }
+        return answer + 1;
+    }
+}
