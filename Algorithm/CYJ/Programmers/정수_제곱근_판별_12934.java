320e6162f24e09bbd85799a532e3d88c8f397c22
yunjichoi9151 <cyjcode99@gmail.com>
2023-08-06 15:59:42 +0900

    [PRGRMS] 정수 제곱근 판별 23.08.06 CYJ

---
+++ Algorithm/CYJ/Programmers/정수_제곱근_판별_12934.java
@@ -0,0 +1,8 @@
+package Programmers;
+
+public class 정수_제곱근_판별_12934 {
+    public long solution(long n) {
+        double answer = Math.sqrt(n);
+        return answer % 1 == 0.0 ? (long)(Math.pow(answer + 1, 2)) : -1;
+    }
+}
