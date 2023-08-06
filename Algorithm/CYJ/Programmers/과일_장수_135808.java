8d8e9686959c8c69b9e9ee27aec69c66ad08ac14
yunjichoi9151 <cyjcode99@gmail.com>
2023-08-06 16:06:38 +0900

    [PRGRMS] 과일 장수 23.08.06 CYJ

---
+++ Algorithm/CYJ/Programmers/과일_장수_135808.java
@@ -0,0 +1,19 @@
+package Programmers;
+
+import java.util.Arrays;
+
+public class 과일_장수_135808 {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        Arrays.sort(score);
+        int now_idx = score.length - 1;
+        while(true) {
+            if(now_idx + 1 - m < 0) {
+                break;
+            }
+            answer += score[now_idx - m + 1] * m;
+            now_idx -= m;
+        }
+        return answer;
+    }
+}
