12917aa35bc84a973fc376d18cff847c3d014a93
yunjichoi9151 <cyjcode99@gmail.com>
2023-08-06 15:59:06 +0900

    [PRGRMS] 자릿수 더하기 23.08.06 CYJ

---
+++ Algorithm/CYJ/Programmers/자릿수_더하기_129831.java
@@ -0,0 +1,15 @@
+package Programmers;
+
+public class 자릿수_더하기_129831 {
+    public int solution(int n) {
+        int answer = 0;
+        while(true) {
+            answer += n % 10;
+            n /= 10;
+            if(n == 0) {
+                break;
+            }
+        }
+        return answer;
+    }
+}
