f25ead3192947b0a94379f86c96f02fee136ea2e
yunjichoi9151 <cyjcode99@gmail.com>
2023-08-06 16:03:05 +0900

    [PRGRMS] 비밀지도 23.08.06 CYJ

---
+++ Algorithm/CYJ/Programmers/비밀지도_17681.java
@@ -0,0 +1,20 @@
+package Programmers;
+
+public class 비밀지도_17681 {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        for(int i = 0; i < n; i++) {
+            String s = Integer.toBinaryString(arr1[i] | arr2[i]);
+            if(s.length() < n) {
+                int k = n - s.length();
+                for(int j = 0; j < k; j++) {
+                    s = "0" + s;
+                }
+            }
+            s = s.replaceAll("1", "#");
+            s = s.replaceAll("0", " ");
+            answer[i] = s;
+        }
+        return answer;
+    }
+}
