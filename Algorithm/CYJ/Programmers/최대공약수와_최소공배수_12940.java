ad45c6706ccdf14e063a990cf1a3fc8e26922b31
yunjichoi9151 <cyjcode99@gmail.com>
2023-08-06 16:00:21 +0900

    [PRGRMS] 최대공약수와 최소공배수 23.08.06 CYJ

---
+++ Algorithm/CYJ/Programmers/최대공약수와_최소공배수_12940.java
@@ -0,0 +1,14 @@
+package Programmers;
+
+public class 최대공약수와_최소공배수_12940 {
+    public int[] solution(int n, int m) {
+        int min = n < m ? n : m;
+        int num1 = 0;
+        for(int i = 1; i <= min; i++) {
+            if(n % i == 0 && m % i == 0) {
+                num1 = i;
+            }
+        }
+        return new int[]{num1, n * m / num1};
+    }
+}
