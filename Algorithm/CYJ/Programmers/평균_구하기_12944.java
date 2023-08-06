cc566b1c832a213ab9d671cc58a57a1384524328
yunjichoi9151 <cyjcode99@gmail.com>
2023-08-06 16:00:46 +0900

    [PRGRMS] 평균 구하기 23.08.06 CYJ

---
+++ Algorithm/CYJ/Programmers/평균_구하기_12944.java
@@ -0,0 +1,11 @@
+package Programmers;
+
+public class 평균_구하기_12944 {
+    public double solution(int[] arr) {
+        double answer = 0;
+        for(int i = 0; i < arr.length; i++) {
+            answer += arr[i];
+        }
+        return answer / arr.length;
+    }
+}
