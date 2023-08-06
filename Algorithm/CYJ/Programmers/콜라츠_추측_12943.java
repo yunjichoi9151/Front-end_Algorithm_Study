3c74ccd4c34fc8142eeb51f680fafce19cbbaa7d
yunjichoi9151 <cyjcode99@gmail.com>
2023-08-06 16:00:35 +0900

    [PRGRMS] 콜라츠 추측 23.08.06 CYJ

---
+++ Algorithm/CYJ/Programmers/콜라츠_추측_12943.java
@@ -0,0 +1,19 @@
+package Programmers;
+
+public class 콜라츠_추측_12943 {
+    public int solution(int num) {
+        int answer = 0;
+        while(num != 1) {
+            if(num % 2 == 0) {
+                num /= 2;
+            } else if(num % 2 == 1) {
+                num = num * 3 + 1;
+            }
+            answer++;
+            if(answer == 500) {
+                return -1;
+            }
+        }
+        return answer;
+    }
+}
