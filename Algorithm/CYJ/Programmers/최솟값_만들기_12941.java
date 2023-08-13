81ae2fa71331c8d02cc05e6abc243bf2c620f0fc
yunjichoi9151 <cyjcode99@gmail.com>
2023-08-13 12:45:53 +0900

    [PRGRMS] 최솟값 만들기 23.08.13 CYJ

---
+++ Algorithm/CYJ/Programmers/최솟값_만들기_12941.java
@@ -0,0 +1,17 @@
+package Programmers;
+
+import java.util.*;
+
+class 최솟값_만들기_12941
+{
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        for(int i = 0; i < A.length; i++) {
+            answer += A[i] * B[A.length - 1 - i];
+        }
+        return answer;
+    }
+}
