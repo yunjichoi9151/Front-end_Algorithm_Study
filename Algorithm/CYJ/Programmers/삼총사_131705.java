cbb1f1ad1813158d3b6c1550ff548af24c71caa6
yunjichoi9151 <cyjcode99@gmail.com>
2023-08-27 16:54:28 +0900

    [PRGRMS] 삼총사 23.08.27 CYJ

---
+++ Algorithm/CYJ/Programmers/삼총사_131705.java
@@ -0,0 +1,16 @@
+class 삼총사_131705 {
+    public int solution(int[] number) {
+        int answer = 0;
+        int l = number.length;
+        for(int i = 0; i < l - 2; i++) {
+            for(int j = i + 1; j < l - 1; j++) {
+                for(int k = j + 1; k < l; k++) {
+                    if(number[i] + number[j] + number[k] == 0) {
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
