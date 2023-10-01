38f512ea9dbfccef63f6624891f9e08ea6c587d2
yunjichoi9151 <cyjcode99@gmail.com>
2023-10-01 16:01:33 +0900

    [BOJ] 베스트셀러 23.10.01 CYJ

---
+++ Algorithm/CYJ/Baekjoon/베스트셀러_1302.java
@@ -0,0 +1,27 @@
+package Baekjoon;
+import java.util.Scanner;
+import java.util.TreeMap;
+import java.util.ArrayList;
+
+public class 베스트셀러_1302 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		String ans = "";
+		int max = 0;
+		TreeMap<String, Integer> map = new TreeMap<>();
+		for(int i = 0; i < n; i++) {
+			String s = sc.next();
+			map.put(s, map.getOrDefault(s, 0) + 1);
+		}
+		ArrayList<String> keySet = new ArrayList<>(map.keySet());
+		for(String key : keySet) {
+			if(map.get(key) > max) {
+				ans = key;
+				max = map.get(key);
+			}
+		}
+		System.out.println(ans);
+		sc.close();
+	}
+}
