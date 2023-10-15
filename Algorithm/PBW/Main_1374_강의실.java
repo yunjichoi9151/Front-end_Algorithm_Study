45cd3a0defd865f60496e685acb043fbf7a12018
pbw <bottler3123@naver.com>
2023-10-15 18:41:19 +0900

    [BOJ] 강의실 23.10.15 PBW

--- Algorithm/PBW/Main_1374_강의실.java
+++ Algorithm/PBW/Main_1374_강의실.java
@@ -16,12 +16,14 @@ public class Main_1374_강의실 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
+		// 수업의 시작시간과 종료시간을 담은 우선순위 큐
 		PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
 			@Override
 			public int compare(int[] o1, int[] o2) {
 				return o1[0] - o2[0];
 			}
 		});
+		// 강의실의 종료시간을 담은 우선순위 큐
 		PriorityQueue<Integer> cnt = new PriorityQueue<>();
 
 		for (int i = 0; i < N; i++) {
@@ -35,11 +37,15 @@ public class Main_1374_강의실 {
 		cnt.add(0);
 		while (!pq.isEmpty()) {
 			int room[] = pq.poll();
+			// 가장 빨리 끝나는 수업의 종료시간을 가져온다
 			int endTime = cnt.poll();
-		
+			
+			// 수업의 시작시간과 가장 빨리 끝나는 수업의 종료시간 비교
 			if (endTime <= room[0]) {
+				// 수업 종료 후에 시작하면 강의실을 추가 안함, 늦게 끝나는 수업 종료 시간을 넣음
 				cnt.add(room[1]);
 			} else {
+				// 수업 종료 전에 시작하면 강의실을 추가함, 각각의 수업 종료 시간을 넣음
 				cnt.add(endTime);
 				cnt.add(room[1]);
 			}
